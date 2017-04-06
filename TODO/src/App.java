fde3a32e67900e4fd2d66d830a2d0eb590858608
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-06 10:28:03 +0200

    update gitignore with *.class - initialize main in App

--- TODO/src/App.java
+++ TODO/src/App.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 /**
  * Created by Gabor on 06/04/2017.
  */
 public class App {
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(args));
+  }
 
 }
--- TODO/src/Todo.java
+++ TODO/src/Todo.java
@@ -2,5 +2,4 @@
  * Created by Gabor on 06/04/2017.
  */
 public class Todo {
-
 }
