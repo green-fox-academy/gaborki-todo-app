6b5c05c3ff8ef4a86a2d8add0016c18f3d0b42c4
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-06 14:36:39 +0200

    create and move listTask() & welcomeMessage() to ToDoList - create isThereArgs() & howManyArgs() to ToDoList

--- TODO/src/App.java
+++ TODO/src/App.java
@@ -12,33 +12,10 @@ import java.util.List;
 public class App {
 
   public static void main(String[] args) {
+    ToDoList myList = new ToDoList(args);
 
-    Path welcomeMessage = Paths.get("welcomeManual.txt");
-    Path taskList = Paths.get("tasks.txt");
-    List<String> welcomeM = null;
-    List<String> tasks = null;
-
-    try {
-      welcomeM = Files.readAllLines(welcomeMessage);
-      if (args.length == 0) {
-        System.out.println();
-        for (String line : welcomeM) {
-          System.out.println(line);
-        }
-        System.out.println();
-      } else if (args.length > 0 && args[0].length() < 3 && args[0].startsWith("-") && args[0].contains("l")) {
-        tasks = Files.readAllLines(taskList);
-        System.out.println();
-        for (int i = 0; i < tasks.size(); i++){
-          System.out.println((i + 1) + " - " + tasks.get(i));
-        }
-        System.out.println();
-      }
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
-
-
+   myList.welcomeMessage();
+   myList.listTask();
   }
 }
 
--- TODO/src/ToDoList.java
+++ TODO/src/ToDoList.java
@@ -1,7 +1,65 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by Gabor on 06/04/2017.
  */
 public class ToDoList {
 
-}
+  Path welcomeMessage = Paths.get("welcomeManual.txt");
+  Path taskList = Paths.get("tasks.txt");
+  List<String> welcomeM;
+  List<String> tasks;
+  String args[];
 
+  boolean isThereArgs(String[] args) {
+    this.args = args;
+    return (args.length > 0);
+  }
+
+  int howManyArgs(String[] args) {
+    this.args = args;
+    if (isThereArgs(this.args)) {
+      return args.length;
+    } else {
+      return 0;
+    }
+  }
+
+  void welcomeMessage() {
+    try {
+      welcomeM = Files.readAllLines(welcomeMessage);
+      if (!isThereArgs(args)) {
+        System.out.println();
+        for (String line : welcomeM) {
+          System.out.println(line);
+        }
+        System.out.println();
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  void listTask(){
+    try {
+      if (isThereArgs(args) && args[0].length() < 3 && args[0].contains("-l")) {
+        tasks = Files.readAllLines(taskList);
+        System.out.println();
+        for (int i = 0; i < tasks.size(); i++) {
+          System.out.println((i + 1) + " - " + tasks.get(i));
+        }
+        System.out.println();
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public ToDoList(String[] args) {
+    this.args = args;
+  }
+}
