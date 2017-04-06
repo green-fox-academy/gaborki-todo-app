d676d7dbd782018e56e56b4e5746ff8789554d81
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-06 12:10:34 +0200

    create welcomeM.txt & App main prints it if there are no arguments

--- TODO/src/App.java
+++ TODO/src/App.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Gabor on 06/04/2017.
@@ -6,7 +12,22 @@ import java.util.Arrays;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
-  }
 
+    Path p = Paths.get("welcomeManual.txt");
+    List<String> welcomeM = null;
+
+    try {
+      welcomeM = Files.readAllLines(p);
+      if (args.length == 0) {
+        System.out.println();
+        for (String line : welcomeM) {
+          System.out.println(line);
+        }
+        System.out.println();
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
 }
+
