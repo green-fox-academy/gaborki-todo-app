029c3fd725d5a9d554436b03cf734aa400e78d7f
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-04-06 13:53:36 +0200

    create tasks.txt & print out tasks with -l

--- TODO/src/App.java
+++ TODO/src/App.java
@@ -13,21 +13,32 @@ public class App {
 
   public static void main(String[] args) {
 
-    Path p = Paths.get("welcomeManual.txt");
+    Path welcomeMessage = Paths.get("welcomeManual.txt");
+    Path taskList = Paths.get("tasks.txt");
     List<String> welcomeM = null;
+    List<String> tasks = null;
 
     try {
-      welcomeM = Files.readAllLines(p);
+      welcomeM = Files.readAllLines(welcomeMessage);
       if (args.length == 0) {
         System.out.println();
         for (String line : welcomeM) {
           System.out.println(line);
         }
         System.out.println();
+      } else if (args.length > 0 && args[0].length() < 3 && args[0].startsWith("-") && args[0].contains("l")) {
+        tasks = Files.readAllLines(taskList);
+        System.out.println();
+        for (int i = 0; i < tasks.size(); i++){
+          System.out.println((i + 1) + " - " + tasks.get(i));
+        }
+        System.out.println();
       }
     } catch (IOException ex) {
       ex.printStackTrace();
     }
+
+
   }
 }
 
